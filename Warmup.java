30a1adffd2ff109291d9013bcb329350218727b4
Karishma Patel <karishma1126@yahoo.com>
2013-09-08 11:02:12 -0600

    added exponent method

--- Warmup.java
+++ Warmup.java
@@ -1,6 +1,7 @@
 public class Warmup {
 	public static void main(String[] args) {
-		factorial(5);
+		// factorial(5);
+		exponent(7,2);
 	}
 	public static int factorial (int n) {
 		int factorial = 1;
@@ -9,5 +10,13 @@ public class Warmup {
 		}
 		System.out.println(factorial);
 		return factorial;
+	}
+	public static int exponent(int x, int n) {
+		int y = 1;
+		for (int i=1; n>=i; i++) {
+			y=y*x;
+		}
+		System.out.println(y);
+		return y;
 	}
 }
