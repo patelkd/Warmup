562c2520566012ce83b9937a664ad1ef2fcd6ba3
Karishma Patel <karishma1126@yahoo.com>
2013-09-08 10:51:32 -0600

    added factorial method

--- Warmup.java
+++ Warmup.java
@@ -1,5 +1,13 @@
 public class Warmup {
 	public static void main(String[] args) {
-		
+		factorial(4);
+	}
+	public static int factorial (int n) {
+		int factorial = 1;
+		for (int i=1; i <=n; i++) {
+			factorial = factorial * i;
+		}
+		System.out.println(factorial);
+		return factorial;
 	}
 }
